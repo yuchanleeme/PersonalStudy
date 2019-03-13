04846b17702bd5d2d05f6b6c35f96ddc13f18ee8
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-14 01:09:27 +0900

    Unity Singlton Practice(C#)
    not using singltone

---
+++ Inflearn/C#_unity/Singleton/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    public Text scoreText;
+    public int Score    // 프로퍼티
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreText.text = value.ToString("N0");
+        }
+    }
+    private int score;
+
+}
---
+++ Inflearn/C#_unity/Singleton/Assets/ScoreUpbtn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpbtn : MonoBehaviour {
+    public GameManager GM; // gameManager 객체 선언후 그 객체 값 증가
+    public void upScore()
+    {
+        GM.Score += 100;
+    }
+
+	
+}
