a8b49002f70d4af7621b69100cff6bfb17178364
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-16 21:41:11 +0900

    change Instance

--- Inflearn/C#_unity/Singleton/Assets/ScoreUpbtn.cs
+++ Inflearn/C#_unity/Singleton/Assets/ScoreUpbtn.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class ScoreUpbtn : MonoBehaviour {
     public void upScore()
     {
-        GameManager.instance.Score += 100; // 싱글톤 인스턴스 선언으로 코드 줄임
+        GameManager.Instance.Score += 100; // 싱글톤 인스턴스 선언으로 코드 줄임
     }
 
 	
