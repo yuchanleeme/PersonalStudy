3806595f2c16bcc6e7b8990a43dedf9745129d4e
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-18 20:13:13 +0900

    studying interface (#2)

---
+++ Inflearn/C#_unity/Interface/Assets/Player_moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_moving : MonoBehaviour {
+    public GameObject test_obj;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            test_obj.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            test_obj.SetActive(true);
+        }
+    }
+}
