169f4b73cfd4333763686cf0ab2b2d0985fd1b0d
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-22 02:45:55 +0900

    caption (#1)

--- Inflearn/C#_unity/Interface/Assets/Box.cs
+++ Inflearn/C#_unity/Interface/Assets/Box.cs
@@ -16,13 +16,7 @@ public class Box : MonoBehaviour {
            
                 GameObject tobj = ObjectPoolManager.Instance.ObjectPool.PopObject();  //오브젝트 풀링을 이용
                 tobj.transform.position = shootPosTf.position;
-
-            if (!tobj)
-            {
-                Debug.Log("NO!");
-                //ObjectPoolManager.Instance.ObjectPool.
-            }
-
+    
 
             //Instantiate(arrowObj).transform.position = shootPosTf.position; // 회전 유지
             //Instantiate(arrowObj, shootPosTf.position, Quaternion.identity); // 회전 없음
