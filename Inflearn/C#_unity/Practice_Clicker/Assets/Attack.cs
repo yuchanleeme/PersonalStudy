2923fa77f6cc8d5bb4592960b669576ffbbdbe8c
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-23 19:50:59 +0900

    button click (#1)

---
+++ Inflearn/C#_unity/Practice_Clicker/Assets/Attack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour {
+    int score = 0;
+    Text scoreText;
+	// Use this for initialization
+	public void ScoreUp(int number)
+    {
+        score += number;
+        GetComponent<Text>().text = "Score :"+score;
+    }
+}
