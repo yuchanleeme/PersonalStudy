57dfc72a3454a16398dc426fa3992dba605c928d
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-29 09:24:31 +0900

    test

--- C/Sudoku.c
+++ C/Sudoku.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-void shuffle(int *arr, int num){ // 대표 값 셔플 함수
+void shuffle_std(int *arr, int num){ // 대표 값 셔플 함수
 	int temp = 0 ;
 	int randomNum = 0;
 	for(int i=0; i< num-1; i++)
@@ -14,13 +14,21 @@ void shuffle(int *arr, int num){ // 대표 값 셔플 함수
 	}
 }
 
-int checkColumn(int (*arr)[9]){
-	int res = 0;
+void shuffle_tmp(int *arr, int num){
+	int temp;
+	temp = arr[num];
+	arr[num] =arr[num+1];
+	arr[num] =temp;
+}
+
+int checkColumn(int (*arr)[9]){	// 같은 열에 같은 숫자가 있는지 비교
+  															// 같은 거 있으면 0리턴
+	int res = 1;
 	for(int i=0; i<9;i++){
 		for(int j=0; j<9;j++){
 			for(int k=1; j+k<10;k++){
 				if (arr[j][i]==arr[j+k][i]) {
-					//printf("arr[%d][%d]==arr[%d][%d]\n",j,i,j+k,i);
+					printf("arr[%d][%d]==arr[%d][%d]\n",j,i,j+k,i);
 					return res;
 				}
 			}
@@ -34,19 +42,15 @@ void madeSudoku(){
 	int StandardNumber[9] = {1,2,3,4,5,6,7,8,9}; // 대표 값을 가진 배열
 	int checkingTrigger = 0;
 
-		while(true){
+
 			if(checkingTrigger==0){
 				for(int i=0; i<9 ;i++){  // 대표 배열을 셔플해서 값들을 본판 맵에 집어 넣음
-					shuffle(StandardNumber,9);
+					shuffle_std(StandardNumber,9);
 					for(int j=0; j<9 ; j++){
 						BaseMap[i][j] = StandardNumber[j];
 						}
 					}
-					checkingTrigger = checkColumn(BaseMap);
 			}
-			else
-				break;
-		}
 
 		for(int k=0; k<9 ;k++){ // 배열에 제대로 들어갔나 확인
 			for(int l=0; l<9 ;l++){
