c3762ca3e8e42d178ae42628fbd9d9cb6245a2ce
yuchanleeme <yuchanlee.me@gmail.com>
2019-03-31 21:56:17 +0900

    Making Sudoku (#4)

--- C/Sudoku.c
+++ C/Sudoku.c
@@ -1,77 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
-void shuffle_std(int *arr, int num){ // 대표 값 셔플 함수
-	int temp = 0 ;
-	int randomNum = 0;
-	for(int i=0; i< num-1; i++)
-	{
-		randomNum = rand()%9;
-		temp = arr[i];
-		arr[i] =arr[randomNum];
-		arr[randomNum] =temp;
-	}
-}
+void madeSudoku();
+bool checkMap(int,int,int);
 
-void shuffle_tmp(int *arr, int num){
-	int temp;
-	temp = arr[num];
-	arr[num] =arr[num+1];
-	arr[num] =temp;
-}
-
-int checkColumn(int (*arr)[9]){	// 같은 열에 같은 숫자가 있는지 비교
-  															// 같은 거 있으면 0리턴
-	int res = 1;
-	for(int i=0; i<9;i++){
-		for(int j=0; j<9;j++){
-			for(int k=1; j+k<10;k++){
-				if (arr[j][i]==arr[j+k][i]) {
-					printf("arr[%d][%d]==arr[%d][%d]\n",j,i,j+k,i);
-					res = 0;
-					return res;
-				}
-			}
-		}
-	}
-	return res;
-}
-
-void madeSudoku(){
-	int BaseMap[9][9]; // 본판 맵
-	int StandardNumber[9] = {1,2,3,4,5,6,7,8,9}; // 대표 값을 가진 배열
-	int checkingTrigger = 0;
-
-while(1){
-			if(checkingTrigger==0){
-				for(int i=0; i<9 ;i++){  // 대표 배열을 셔플해서 값들을 본판 맵에 집어 넣음
-					shuffle_std(StandardNumber,9);
-					for(int j=0; j<9 ; j++){
-						if(checkColumn(BaseMap)==0){
-							shuffle_tmp(StandardNumber,j);
-							printf("tmp shuffle_tmp\n");
-							for(int k=0; k<9 ; k++){
-								printf("%d ", StandardNumber[k]);
-							}
-							printf("\n");
-						}
-						BaseMap[i][j] = StandardNumber[j];
-						}
-						}
-					}
-			}
-
-		for(int k=0; k<9 ;k++){ // 배열에 제대로 들어갔나 확인
-			for(int l=0; l<9 ;l++){
-			printf("%d ",BaseMap[k][l]);
-			}
-  		printf("\n");
-		}
-}
+int BaseMap[9][9];
 
 int main() {
 	srand(time(NULL));
 	madeSudoku(); // making Sudoku
   return 0;
+}
+
+void madeSudoku(){
+
+	int iRow,iCol,tempValue;
+	int checkingTrigger = true;
+
+	for(int iRow =0; iRow < 9; iRow++){
+		//printf("<1>irow = %d, iCol= %d \n",iRow,iCol);
+		for (int iCol = 0; iCol < 9; iCol++) {
+			//printf("<2>irow = %d, iCol= %d \n",iRow,iCol);
+			while(checkingTrigger == true){
+				tempValue = rand()%9 +1;
+				if(checkMap(iRow,iCol,tempValue) == true){
+					BaseMap[iRow][iCol] = tempValue;
+					break;
+				}
+			}
+		}
+	}
+
+
+	for(int k=0; k<9 ;k++){ // 배열에 제대로 들어갔나 확인
+		for(int l=0; l<9 ;l++){
+		printf("%d ",BaseMap[k][l]);
+		}
+		printf("\n");
+	}
+}
+bool checkMap(int row, int col, int value){
+
+	for(int i = 0; i<9 ; i++){
+		if(BaseMap[row][i] == value){
+			return false;
+		}
+	}
+	for(int j = 0; j<9 ; j++){
+		if(BaseMap[j][col] == value){
+			return false;
+		}
+	}
+	int stdRow = (row/3)*3;
+	int stdCol = (col/3)*3;
+	for(int x = 0; x<3 ; x++){
+		for(int y = 0; y<3 ; y++){
+			if(BaseMap[stdRow+x][stdCol+y]==value){
+				return false;
+			}
+		}
+	}
+	return true;
 }
