0f26b341d2d982c6f2fe7916d8067bb4c13d24c1
yuchanleeme <yuchanlee.me@gmail.com>
2019-04-22 01:00:22 +0900

    Baek Algorithm (#6)

--- Baek_algo/baek_1018.cpp
+++ Baek_algo/baek_1018.cpp
@@ -3,13 +3,21 @@
 using namespace std;
 
 int main(){
-    //https://www.acmicpc.net/problem/1018
+<<<<<<< HEAD
+  //https://www.acmicpc.net/problem/1018
+=======
+>>>>>>> be2e0963139d04063392185fa7c428edab4a1952
   char checking_white[8][8];
   char checking_black[8][8];
 
   int N,M;
+<<<<<<< HEAD
   int count_b = 0;
   int count_w = 0;
+  int count_res = 100;
+=======
+
+>>>>>>> be2e0963139d04063392185fa7c428edab4a1952
 
   for (int i = 0 ; i < 8 ; i++){
     for (int j = 0; j < 8 ; j++){
@@ -36,24 +44,47 @@ int main(){
   cin >> N >> M;
 
   char input_map[N][M];
-
+<<<<<<< HEAD
+=======
+  char
+>>>>>>> be2e0963139d04063392185fa7c428edab4a1952
 
   for(int i = 0; i<N ; i++){ // 맵 입력
      for(int j = 0; j<M; j++){
       cin >> input_map[i][j];
      }
    }
-   for(int k = 0; k < N-7; k++){
-     for()
-   }
-  for(int x = 0 ; x < 8 ; x++){
-    for(int y = 0; y < 8 ; y++){
-      if(input_map[x+i][y+j]!=checking_black[x][y]){
-        count_b++;
-      }
-    }
+<<<<<<< HEAD
+  for(int i = 0; i < N-7; i++){
+     for (int j = 0; j < M-7; j++) {
 
-  }
+       for(int x = 0 ; x < 8 ; x++){
+         for(int y = 0; y < 8 ; y++){
+           if(input_map[x+i][y+j]!=checking_black[x][y]){
+             count_b++;
+           }
+           if(input_map[x+i][y+j]!=checking_white[x][y]){
+             count_w++;
+           }
+         }
+       }
+       if(count_res > count_b){
+         count_res = count_b;
+       }
+       if(count_res > count_w){
+         count_res = count_w;
+       }
+       count_b = 0;
+       count_w = 0;
+
+     }
+   }
+
+  cout<<count_res<<endl;
+
+=======
+
+  for()
 
   for(int i = 0; i<N ; i++){
        for(int j = 0; j<M; j++){
@@ -61,5 +92,6 @@ int main(){
        }
        cout<<endl;
   }
+>>>>>>> be2e0963139d04063392185fa7c428edab4a1952
   return 0;
 }
