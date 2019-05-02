b531cc0c16a9b871550b0af9cb0b170a8d28b08c
yuchanleeme <yuchanlee.me@gmail.com>
2019-05-02 18:14:48 +0900

    Baek Algorithm (#18)

--- Baek_algo/baek_11719.cpp
+++ Baek_algo/baek_11719.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-// https://www.acmicpc.net/problem/11718
+// https://www.acmicpc.net/problem/11719
 
 /*
-<그대로 출력하기>
+<그대로 출력하기 2>
 주요: getline의 이해
 1. string 값 text를 할당한다.
 2. EOF 까지 계속 출력한다.
