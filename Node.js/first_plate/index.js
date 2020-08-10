398f1d95099762b11c3caa4aa59a6a0e2fb2ba9c
yuchanleeme <48426909+yuchanleeme@users.noreply.github.com>
2020-08-10 12:02:20 +0900

    Security & Password

--- Node.js/first_plate/index.js
+++ Node.js/first_plate/index.js
@@ -1,14 +1,41 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
 
+const {User} = require("./models/User");
+
+// application/x-www-form-urlencoded
+// 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({extended: true}));
+
+// applicaion/json
+// json 파일을 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://lee:0315@firstplate.ubs55.mongodb.net/mydb?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! here'))
+
+// post를 보내준다.
+app.post('/register', (req, res) => {
+
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.  
+  const user = new User(req.body)
+  // console.log("req.body : " , req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- Node.js/first_plate/models/User.js
+++ Node.js/first_plate/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -24,6 +26,30 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: { // 토큰 유효기간
         type: Number
+    }
+})
+
+// 저장하기 전에 한다.
+userSchema.pre('save', function (next){
+    var user = this;
+
+    //비밀번호를 바꿀때만 암호화 해준다.
+    if(user.isModified('password')){
+        // 비밀번호를 암호화 한다.
+  
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            // myPlaintextPassword: 순수 비밀번호
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                // store hash in your password DB.
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
     }
 })
 
