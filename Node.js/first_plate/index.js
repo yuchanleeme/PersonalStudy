b3af05b0ee5e9b483407c68ed993c5a76b9fc8cc
yuchanleeme <48426909+yuchanleeme@users.noreply.github.com>
2020-08-10 15:12:24 +0900

    Login & Logout

--- Node.js/first_plate/index.js
+++ Node.js/first_plate/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 const config = require('./config/key');
-
+const {auth} = require('./middleware/auth')
 const {User} = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // applicaion/json
 // json 파일을 분석해서 가져올 수 있게 해줌
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -23,7 +25,7 @@ mongoose.connect(config.mongoURI,{
 app.get('/', (req, res) => res.send('Hello World! here'))
 
 // post를 보내준다.
-app.post('/register', (req, res) => {
+app.post('/api/user/register', (req, res) => {
 
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.  
@@ -36,6 +38,58 @@ app.post('/register', (req, res) => {
     })
   })
 
+})
+
+// 로그인 라우터
+app.post('/api/user/login', (req, res) => {
+  // 1. 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user){
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    // 2. 요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) =>{
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+        // 3. 비밀번호까지 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) =>{
+        if(err) return res.status(400).senc(err);
+
+        // 토큰을 (쿠키, 로컬스토리지 등) 에 저장한다.
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId : user._id})
+      })
+    })
+  })
+})
+
+app.get('/api/user/auth', auth, (req, res) =>{
+  // 여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+// 로그 아웃
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+    {token: ""}, (err, user) =>{
+      if(err) return res.json({success: false, err})
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- Node.js/first_plate/models/User.js
+++ Node.js/first_plate/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -52,6 +53,41 @@ userSchema.pre('save', function (next){
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // plainPassword 1234567 (기존 비밀번호를 암호화해서 비교)
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    // jsonwebToken을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+        user.findOne({"_id" : decoded, "token" : token}, function(err,user){
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User',userSchema)
 
